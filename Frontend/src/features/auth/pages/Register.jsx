2f8c7e7422d82f36801e145abdccb41d4f59ecae
Ayush Jaiswal <ayushjai8@gmail.com>
2026-08-21 14:11:42 +0530

    making website phone friendly

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate, Link } from 'react-router'
+import "../auth.form.scss"
 import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
